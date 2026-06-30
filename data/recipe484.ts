e215f3b094b3ec7bbbdbc3110602a6e484cf605c
ahrong0820 <ahrong0820@gmail.com>
2026-06-30 19:52:54 +0900

    feat: expand 484 metadata

--- data/recipe484.ts
+++ data/recipe484.ts
@@ -1,1 +1,8 @@
-export const recipe484 = { id: "jis-484-15g-2026" };
+export const recipe484 = {
+  id: "jis-484-15g-2026",
+  name: "정인성 484 15g (2026)",
+  method: "V60",
+  dose: 15,
+  water: 220,
+  totalTime: 170,
+};
